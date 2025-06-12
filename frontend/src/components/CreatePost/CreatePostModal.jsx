e6067633c11aa14e55c211225445d19174388d09
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-12 23:47:02 +0530

    post add image action finish, image upload to cloudinary & show a preview in webapp

--- frontend/src/components/CreatePost/CreatePostModal.jsx
+++ frontend/src/components/CreatePost/CreatePostModal.jsx
@@ -1,37 +1,140 @@
-import React from 'react';
-import { Modal, Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Modal,
+  Box,
+  Avatar,
+  IconButton,
+  Button,
+  Backdrop,
+  CircularProgress,
+} from "@mui/material";
+import { useFormik } from "formik";
+import ImageIcon from "@mui/icons-material/Image";
+import VideoIcon from "@mui/icons-material/VideoCall";
+import { uploadToCloudinary } from "../../utils/uploadToCloudinary";
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 500,
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
-    borderRadius:".6rem",
-    outline:"none",
-  };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: ".6rem",
+  outline: "none",
+};
 
-const CreatePostModal = ({handleClose, open}) => {
+const CreatePostModal = ({ handleClose, open }) => {
+  const [selectedImage, setSelectedImage] = useState();
+  const [selectedVideo, setSelectedVideo] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSelectImage = async (event) => {
+    setIsLoading(true);
+    const imageUrl = await uploadToCloudinary(event.target.files[0], "image");
+    setSelectedImage(imageUrl);
+    setIsLoading(false);
+    formik.setFieldValue("image", imageUrl);
+  };
+  const handleSelectVideo = () => {};
+
+  const formik = useFormik({
+    initialValues: {
+      caption: "",
+      image: "",
+      video: "",
+    },
+    onSubmit: (values) => {
+      console.log("formik values", values);
+    },
+  });
+
   return (
     <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-  )
-}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <form onSubmit={formik.handleSubmit}>
+          <div>
+            <div className="flex space-x-4 items-center">
+              <Avatar />
+              <div>
+                <p className="font-bold text-lg">V.S. Gunasekara</p>
+                <p className="text-sm">@it21320378</p>
+              </div>
+            </div>
+            <textarea
+              className="outline-none w-full mt-5 p-2 bg-transparent border border-[#3b4054] rounded-sm"
+              placeholder="write caption..."
+              name="caption"
+              id=""
+              onChange={formik.handleChange}
+              value={formik.values.caption}
+              rows="4"
+            ></textarea>
+            <div className="flex space-x-5 items-center mt-5">
+              <div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleSelectImage}
+                  style={{ display: "none" }}
+                  id="image-input"
+                />
+                <label htmlFor="image-input">
+                  <IconButton color="primary" component="span">
+                    <ImageIcon />
+                  </IconButton>
+                </label>
+                <span>Image</span>
+              </div>
+              <div>
+                <input
+                  type="file"
+                  accept="video/*"
+                  onChange={handleSelectVideo}
+                  style={{ display: "none" }}
+                  id="video-input"
+                />
+                <label htmlFor="video-input">
+                  <IconButton color="primary">
+                    <VideoIcon />
+                  </IconButton>
+                </label>
+                <span>Video</span>
+              </div>
+            </div>
+            {selectedImage && (
+              <div>
+                <img className="h-[10rem]" src={selectedImage} alt="" />
+              </div>
+            )}
+            <div className="flex w-full justify-end">
+              <Button
+                variant="contained"
+                type="submit"
+                sx={{ borderRadius: "1.5rem" }}
+              >
+                Post
+              </Button>
+            </div>
+          </div>
+        </form>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={isLoading}
+          onClick={handleClose}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </Box>
+    </Modal>
+  );
+};
 
 export default CreatePostModal;
