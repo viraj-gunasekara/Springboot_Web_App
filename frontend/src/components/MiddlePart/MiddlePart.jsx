d6709e176095478e39d6c390699126cb8c2d847b
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-12 23:43:39 +0530

    create post modal to open when click home post section

--- frontend/src/components/MiddlePart/MiddlePart.jsx
+++ frontend/src/components/MiddlePart/MiddlePart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, Card, IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import StoryCircle from "./StoryCircle";
@@ -6,14 +6,18 @@ import ImageIcon from '@mui/icons-material/Image';
 import VideocamIcon from '@mui/icons-material/Videocam';
 import ArticleIcon from '@mui/icons-material/Article';
 import PostCard from "../Post/PostCard";
+import CreatePostModal from "../CreatePost/CreatePostModal";
 
 const story = [1, 1, 1, 1, 1];
 const posts = [1, 1, 1, 1, 1];
 
 const MiddlePart = () => {
+  const [openCreatePostModal, setOpenCreatePostModal]=useState(false);
 
+  const handleCloseCreatePostModal=()=>setOpenCreatePostModal(false);
   const handleOpenCreatePostModal=()=>{
-    console.log("open post model...")
+    setOpenCreatePostModal(true);
+    console.log("open post model...", openCreatePostModal);
   }
   return (
     <div className="px-20">
@@ -33,6 +37,7 @@ const MiddlePart = () => {
         <div className="flex justify-between">
           <Avatar />
           <input
+          onClick={handleOpenCreatePostModal}
             readOnly
             type="text"
             className="outline-none w-[90%] rounded-full px-5 bg-transparent border-[#3b4054] border"
@@ -66,6 +71,9 @@ const MiddlePart = () => {
       {/* section3 */}
       <div className="mt-5 space-y-5 mb-5">
           {posts.map((item)=><PostCard/>)}
+      </div>
+      <div>
+        <CreatePostModal handleClose={handleCloseCreatePostModal} open={openCreatePostModal}/>
       </div>
     </div>
   );
