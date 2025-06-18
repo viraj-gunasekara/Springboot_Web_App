38769de8546f10c51e055faa4955c784d890fe9e
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-18 22:38:13 +0530

    comment button functionality, when click, comment write section open with showing others comments

--- frontend/src/components/Post/PostCard.jsx
+++ frontend/src/components/Post/PostCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { red } from "@mui/material/colors";
 import {
   Card,
@@ -9,6 +9,7 @@ import {
   CardContent,
   Typography,
   CardActions,
+  Divider,
 } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -18,7 +19,10 @@ import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
-const PostCard = () => {
+const PostCard = ({item}) => {
+const [showComments, setShowComments]=useState(false);
+const handleShowComment=()=>setShowComments(!showComments);
+
   return (
     <Card>
       <CardHeader
@@ -54,7 +58,7 @@ const PostCard = () => {
           <IconButton color="error">
             {true ? <FavoriteIcon /> : <FavoriteBorderIcon />}
           </IconButton>
-          <IconButton>{<ChatBubbleIcon />}</IconButton>
+          <IconButton onClick={handleShowComment}>{<ChatBubbleIcon />}</IconButton>
           <IconButton color="primary">{<ShareIcon />}</IconButton>
         </div>
 
@@ -64,6 +68,33 @@ const PostCard = () => {
           </IconButton>
         </div>
       </CardActions>
+      {showComments && <section>
+        <div className="flex items-center space-x-5 mx-3 my-5">
+          <Avatar sx={{}} />
+
+          <input onKeyPress={(e)=>{
+            if(e.key==="Enter"){
+              console.log("enter pressed ---", e.target.value)
+            }
+          }}
+            placeholder="write your comment..."
+            className="w-full outline-none bg-transparent border border-[#3b4054] rounded-full px-5 py-2"
+            type="text"
+          />
+        </div>
+        <Divider/>
+        <div className="mx-3 space-y-2 my-5 text-xs">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center space-x-5">
+              <Avatar sx={{height:"2rem", width:"2rem", forntSize:".8rem"}}>
+                    P
+              </Avatar>
+              <p>nice image</p>
+            </div>
+
+          </div>
+        </div>
+      </section>}
     </Card>
   );
 };
