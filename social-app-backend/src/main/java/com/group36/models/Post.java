dd7b10065bcee7339dfc3f0a2969d09533ee8e33
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-02 23:51:23 +0530

    declare backend methods for Posts, create post, delete post, find post byId, save post, like post

---
+++ social-app-backend/src/main/java/com/group36/models/Post.java
@@ -0,0 +1,114 @@
+package com.group36.models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer Id;
+	
+	private String caption;
+	
+	private String image;
+	
+	private String video;
+	
+	private User user;
+	
+	private LocalDateTime createdAt;
+	
+	private List<User> liked=new ArrayList<>();
+	
+	
+	public Post() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Post(Integer id, String caption, String image, String video, User user, LocalDateTime createdAt,
+			List<User> liked) {
+		super();
+		Id = id;
+		this.caption = caption;
+		this.image = image;
+		this.video = video;
+		this.user = user;
+		this.createdAt = createdAt;
+		this.liked = liked;
+	}
+
+
+
+	public List<User> getLiked() {
+		return liked;
+	}
+
+
+
+	public void setLiked(List<User> liked) {
+		this.liked = liked;
+	}
+
+
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getVideo() {
+		return video;
+	}
+
+	public void setVideo(String video) {
+		this.video = video;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+ 
+}
